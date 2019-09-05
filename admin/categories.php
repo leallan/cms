7f19cdac9c3419997f1d836ce6ae0913d277d373
allan <allanle@shaw.ca>
2019-09-04 22:38:34 -0700

    added add category function

--- admin/categories.php
+++ admin/categories.php
@@ -17,7 +17,25 @@
                      </h1>
 
                      <div class="col-xs-6">
-                        <form action="">
+                         <?php 
+                            if(isset($_POST['submit'])) {
+                                $cat_title = $_POST['cat_title'];
+
+                                if($cat_title == "" || empty($cat_title)) {
+                                    echo "This field should not be empty";
+                                } else {
+                                    $query = "INSERT INTO categories(cat_title)";
+                                    $query .= "VALUE('{$cat_title}')";
+
+                                    $create_category_query = mysqli_query($connection, $query);
+                                    
+                                    if(!$create_category_query) {
+                                        die('Query Failed' . mysqli_error($connection));
+                                    }
+                                }
+                            }
+                         ?>
+                        <form action="" method="post">
                             <div class="form-group">
                                 <label for="cat_title">Add Category</label>
                                 <input class="form-control" type="text" name="cat_title">
@@ -26,6 +44,33 @@
                                 <input class="btn btn-primary" type="submit" name="submit" value="Add Category">
                             </div>
                         </form>
+                    </div>
+                    <div class="col-xs-6">
+                        <?php
+                            $query = "SELECT * FROM categories";
+                            $select_categories = mysqli_query($connection, $query);
+                        ?>
+                        <table class="table table-bordered table-hover">
+                            <thead>
+                                <tr>
+                                    <th>Id</th>
+                                    <th>Category Title</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                    while($row = mysqli_fetch_assoc($select_categories)) {
+                                        $cat_id = $row['cat_id'];
+                                        $cat_title = $row['cat_title'];
+                                                    
+                                        echo "<tr>";
+                                        echo "<td>{$cat_title}</td>";
+                                        echo "<td>{$cat_id}</td>";
+                                        echo "</tr>";
+                                    }
+                                ?>
+                            </tbody>
+                        </table>
                     </div>
                  </div>
              </div>
