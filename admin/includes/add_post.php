bacb5605a5a764d55dbf469292ffd67a94727bc7
allan <allanle@shaw.ca>
2019-09-09 14:18:48 -0700

    category update

--- admin/includes/add_post.php
+++ admin/includes/add_post.php
@@ -2,7 +2,7 @@
     if(isset($_POST['create_post'])) {
         $post_title = $_POST['title'];
         $post_author = $_POST['author'];
-        $post_category_id = $_POST['post_category_id'];
+        $post_category_id = $_POST['post_category'];
         $post_status = $_POST['post_status'];
 
         $post_image = $_FILES['image']['name'];
@@ -30,8 +30,34 @@
         <input type="text" class="form-control" name="title">
     </div>
     <div class="form-group">
-        <label for="post_category">Post Category Id</label>
-        <input type="text" class="form-control" name="post_category_id">
+        <select name="post_category" id="post_category">
+            <?php
+            $query = "SELECT * FROM categories";
+            $select_categories = mysqli_query($connection, $query);
+
+            confirmQuery($select_categories);
+
+            while ($row = mysqli_fetch_assoc($select_categories)) {
+                $cat_id = $row['cat_id'];
+                $cat_title = $row['cat_title'];
+
+                echo "<option value='{$cat_id}'>{$cat_title}</option>";
+            }
+
+            if (isset($_POST['update_post'])) {
+                $post_title = $_POST['post_title'];
+                $post_author = $_POST['post_author'];
+                $post_category_id = $_POST['post_category_id'];
+                $post_status = $_POST['post_status'];
+
+                $post_image = $_FILES['image']['name'];
+                $post_image_temp = $_FILES['image']['tmp_name'];
+
+                $post_tags = $_POST['post_tags'];
+                $post_content = $_POST['post_content'];
+            }
+            ?>
+        </select>
     </div>
     <div class="form-group">
         <label for="title">Post Author</label>
