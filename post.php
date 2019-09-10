ba8de2d17f904bfd29d9f7cfacd124dc3f90ed47
allan <allanle@shaw.ca>
2019-09-09 20:13:05 -0700

    refactored and added comments section

--- post.php
+++ post.php
@@ -12,7 +12,7 @@
         <!-- Blog Entries Column -->
         <div class="col-md-8">
             <?php
-            if(isset($_GET['p_id'])) {
+            if (isset($_GET['p_id'])) {
                 $the_post_id = $_GET['p_id'];
             }
 
@@ -42,15 +42,82 @@
                 <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
                 <hr>
             <?php } ?>
+
+            <!-- Blog Comments -->
+            <?php
+            if (isset($_POST['create_comment'])) {
+                echo $_POST['comment_author'];
+            }
+            ?>
+            <!-- Comments Form -->
+            <div class="well">
+                <h4>Leave a Comment:</h4>
+                <form action="" method="post" role="form">
+                    <div class="form-group">
+                        <label for="Author">Author</label>
+                        <input type="text" class="form-control" name="comment_author">
+                    </div>
+                    <div class="form-group">
+                        <label for="Email">Email</label>
+                        <input type="email" class="form-control" name="comment_email">
+                    </div>
+                    <div class="form-group">
+                        <label for="Comment">Your Comment</label>
+                        <textarea class="form-control" rows="3"></textarea>
+                    </div>
+                    <button type="submit" name="create_comment" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+
+            <hr>
+
+            <!-- Posted Comments -->
+
+            <!-- Comment -->
+            <div class="media">
+                <a class="pull-left" href="#">
+                    <img class="media-object" src="http://placehold.it/64x64" alt="">
+                </a>
+                <div class="media-body">
+                    <h4 class="media-heading">Start Bootstrap
+                        <small>August 25, 2014 at 9:30 PM</small>
+                    </h4>
+                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                </div>
+            </div>
+
+            <!-- Comment -->
+            <div class="media">
+                <a class="pull-left" href="#">
+                    <img class="media-object" src="http://placehold.it/64x64" alt="">
+                </a>
+                <div class="media-body">
+                    <h4 class="media-heading">Start Bootstrap
+                        <small>August 25, 2014 at 9:30 PM</small>
+                    </h4>
+                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                    <!-- Nested Comment -->
+                    <div class="media">
+                        <a class="pull-left" href="#">
+                            <img class="media-object" src="http://placehold.it/64x64" alt="">
+                        </a>
+                        <div class="media-body">
+                            <h4 class="media-heading">Nested Start Bootstrap
+                                <small>August 25, 2014 at 9:30 PM</small>
+                            </h4>
+                            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                        </div>
+                    </div>
+                    <!-- End Nested Comment -->
+                </div>
+
+            </div>
         </div>
+        <!-- /.row -->
 
         <!-- Blog Sidebar Widgets Column -->
         <?php include 'includes/sidebar.php' ?>
+        <hr>
 
-    </div>
-    <!-- /.row -->
-
-    <hr>
-
-    <!-- Footer -->
-    <?php include 'includes/footer.php' ?>
+        <!-- Footer -->
+        <?php include 'includes/footer.php' ?>
