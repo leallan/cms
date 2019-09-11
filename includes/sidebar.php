f3de2a565ea1645583d8d907c8b29a90acc83402
allan <allanle@shaw.ca>
2019-09-10 23:22:28 -0700

    added login form and validation of user

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -10,7 +10,20 @@
         </form>
         <!-- /.input-group -->
     </div>
-
+    <!-- Login -->
+    <div class="well">
+        <h4>Login</h4>
+        <form action="includes/login.php" method="post">
+            <div class="form-group">
+                <input name="username" type="text" class="form-control" placeholder="Enter User Name">
+            </div>
+            <div class="input-group">
+                <input name="password" type="password" class="form-control" placeholder="Enter Password">
+                <span class="input-group-btn"><button class="btn btn-primary" name="login" type="submit">Submit</button></span>
+            </div>
+        </form>
+        
+    </div>
     <!-- Blog Categories Well -->
     <div class="well">
         <?php
