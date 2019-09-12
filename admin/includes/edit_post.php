0b998e6be779d2d939aa05944c7581f276bba4c7
allan <allanle@shaw.ca>
2019-09-12 10:32:19 -0700

    add update notification and updated more links

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -55,6 +55,8 @@
         $update_post = mysqli_query($connection, $query);
 
         confirmQuery($update_post);
+
+        echo "<p class='bg-success'>Post Updated. <a href='../post.php?p_id={$post_id}'>View Post</a> or <a href='posts.php'>Edit More Post</a></p>";
     }
 ?>
 
