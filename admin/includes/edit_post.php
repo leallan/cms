03c221e83ab56a00397dda5e0a139140fb149d59
allan <allanle@shaw.ca>
2019-09-11 21:54:34 -0700

    fixed broken links and add new body

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -98,9 +98,21 @@
         <input value="<?php echo $post_author; ?>" type="text" class="form-control" name="post_author">
     </div>
     <div class="form-group">
+        <select name="post_status" id="">
+            <option value="<?php echo $post_status ?>"><?php echo $post_status; ?></option>
+            <?php
+            if($post_status == 'published') {
+               echo "<option value='draft'>Draft</option>";
+            } else {
+                echo "<option value='published'>Publish</option>";
+            }
+            ?>
+        </select>
+    </div>
+    <!-- <div class="form-group">
         <label for="post_status">Post Status</label>
         <input value="<?php echo $post_status; ?>" type="text" class="form-control" name="post_status">
-    </div>
+    </div> -->
     <div class="form-group">
         <img src="../images/<?php echo $post_image; ?>" alt="" width="100">
         <input type="file" name="image">
